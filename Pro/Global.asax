976c6198059aedbe791a9174b55ccf706625d29a
thanhlong <doanthanhlong1801@gmail.com>
2018-10-03 11:03:56 +0700

    Add project files.

---
+++ Pro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pro.MvcApplication" Language="C#" %>
---
+++ Pro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pro.Startup))]
+namespace Pro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
